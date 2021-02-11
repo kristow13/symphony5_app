fb085f45366165c7fded7e2a58689c471ef849f4
web <kristow13@gmail.com>
2021-02-11 23:19:08 +0300

    init

---
+++ tests/Manual/MyTest.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace App\Tests\Manual;
+
+
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class MyTest extends KernelTestCase
+{
+    public function testMyTest()
+    {
+        return;
+    }
+}
